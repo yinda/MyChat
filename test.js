859e7856934e7012b3376ad1fbf117403938a583
yinda <yinda1987@hotmail.com>
2017-08-09 19:03:49 -0700

    test

--- test.js
+++ test.js
@@ -7,6 +7,11 @@ var t='IMG haha';
 
 var r = t.replace('IMG', 'iimg');
 
+var i = 'C:\\Program Files\\VanDyke Software\\Clients\\;%JAVA_HOME%\\bin;C:\\Program Files\\Common Files\\Microsoft Shared\\Windows Live;C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\Windows Live;C:\\Program Files (x86)\\Intel\\iCLS Client\\;C:\\Program Files\\Intel\\iCLS Client\\;%SystemRoot%\\system32;%SystemRoot%;%SystemRoot%\\System32\\Wbem;%SYSTEMROOT%\\System32\\WindowsPowerShell\\v1.0\\;C:\\Program Files\\Intel\\Intel(R) Management Engine Components\\DAL;C:\\Program Files\\Intel\\Intel(R) Management Engine Components\\IPT;C:\\Program Files (x86)\\Intel\\Intel(R) Management Engine Components\\DAL;C:\\Program Files (x86)\\Intel\\Intel(R) Management Engine Components\\IPT;C:\\Program Files (x86)\\Intel\\OpenCL SDK\\2.0\\bin\x86;C:\\Program Files (x86)\\Intel\\OpenCL SDK\\2.0\\bin\x64;C:\\Program Files\\Common Files\\Lenovo;C:\\Program Files (x86)\\Common Files\\Ulead Systems\\MPEG;C:\\Program Files (x86)\\Windows Live\\Shared;C:\\SWTOOLS\\ReadyApps;C:\\Program Files (x86)\\Symantec\\VIP Access Client\\;C:\\Program Files (x86)\\Common Files\\Lenovo;C:\\Program Files\\Intel\\WiFi\\bin\\;C:\\Program Files\\Common Files\\Intel\\WirelessCommon\\;C:\\Program Files (x86)\\Google\\Google Apps Sync\\;C:\\Program Files (x86)\\Google\\Google Apps Migration\\;C:\\shortcuts;C:\\Program Files\\TortoiseSVN\\bin;C:\\Python27;C:\\Program Files (x86)\\Skype\\Phone\\;C:\\windows\\system32;C:\\windows;C:\\windows\\System32\\Wbem;C:\\Program Files\\Java\\jdk1.8.0_101\\bin;C:\\Program Files\\nodejs\\;%MAVEN_HOME%\\bin;';
+
+var _i = i.split(';');
+console.log(_i);
+
 console.log('test');
 
 console.log(r);
